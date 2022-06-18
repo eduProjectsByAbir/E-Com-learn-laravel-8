b2e8f0f4835354e0c3e4462b12ec9d6a457bcd69
Abir <abiruzzaman.molla@gmail.com>
2022-06-18 15:18:42 +0600

    Added a Procfile.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,16 +26,24 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         $categories = Category::with('subcategories', 'subcategories.subSubcategories')->latest('id')->get();
-        view()->share('categories', $categories);
+        if($categories){
+            view()->share('categories', $categories);
+        }
 
         $featuredproducts = Product::where('status', 1)->where('featured', 1)->latest('id')->limit(6)->get();
-        view()->share('featuredproducts', $featuredproducts);
+        if($featuredproducts){
+            view()->share('featuredproducts', $featuredproducts);
+        }
 
         $hotproducts = Product::where('status', 1)->where('hot_deals', 1)->latest('id')->limit(6)->get();
-        view()->share('hotproducts', $hotproducts);
+        if($hotproducts){
+            view()->share('hotproducts', $hotproducts);
+        }
 
         $tags = Product::groupBy('tags')->pluck('tags')->implode(', ');
-        $tags = (array_map('trim', array_unique(explode(',', $tags))));
-        view()->share('tags', $tags);
+        if($tags){
+            $tags = (array_map('trim', array_unique(explode(',', $tags))));
+            view()->share('tags', $tags);
+        }
     }
 }
